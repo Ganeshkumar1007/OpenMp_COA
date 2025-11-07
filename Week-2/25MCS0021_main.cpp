e4a06bb2bed828ef58c855acddf4ccbe2c486395
Ganesh Kumar R <78259569+Ganeshkumar1007@users.noreply.github.com>
2025-11-08 01:07:24 +0530

    Delete Week-2 directory

--- Week-2/25MCS0021_main.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<omp.h>
-#include "mnudrvprg_010_parallel_construct.h"
-int main(){
-
-    fn_mnudrvprg_010_parallel_construct();
-
-    return 0;
-}
--- Week-2/helloworld_serial.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<stdio.h>
-
-void fn_helloworld_serial()
-{
-	printf("Hello World Serial\n");
-	
-}
