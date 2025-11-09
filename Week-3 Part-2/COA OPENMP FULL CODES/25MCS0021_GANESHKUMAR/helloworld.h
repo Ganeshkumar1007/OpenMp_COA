e36462d00c266e2a51cfafa095da260d3f30c035
Ganesh Kumar R <78259569+Ganeshkumar1007@users.noreply.github.com>
2025-11-09 13:02:18 +0530

    Add files via upload

---
+++ Week-3 Part-2/COA OPENMP FULL CODES/25MCS0021_GANESHKUMAR/helloworld.h
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void fn_helloworld() {
+    printf("Hello World");
+    
+}
---
+++ Week-3 Part-2/COA OPENMP FULL CODES/25MCS0021_GANESHKUMAR/helloworld_serial.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<stdio.h>
+
+void fn_helloworld_serial()
+{
+	printf("Hello World Serial\n");
+	
+}
